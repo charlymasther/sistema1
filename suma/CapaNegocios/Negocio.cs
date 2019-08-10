7e3b5b02af2c95ee406292c780769adf459e4525
charly <paredescj2018@gmail.com>
2019-08-09 20:12:22 -0500

    agregando proyecto

---
+++ suma/CapaNegocios/Negocio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CapaDatos;
+using CapaEntidad;
+
+
+namespace CapaNegocios
+{
+    public class Negocio
+    {
+        Datos dts = new Datos();
+        public double N_suma(Entidad obj)
+        {
+            return dts.D_suma(obj);
+        
+        }
+
+
+    }
+}
